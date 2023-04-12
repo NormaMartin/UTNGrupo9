38ab9d70926129a23c9e424d0525fbd1f89537d0
NormaMartin <128730488+NormaMartin@users.noreply.github.com>
2023-04-11 21:21:03 -0300

    formato postulate

--- version10.04.2023/formulario(para postulate)/indexform.js
+++ version10.04.2023/formulario(para postulate)/indexform.js
@@ -1,31 +1,55 @@
-const form = document.getElementById("laboral");
-form.addEventListener ("submit", agarraform);
+const nombre = document.getElementById ("nombre");
+const telefono = document.getElementById ("telefono");
+const mail = document.getElementById ("mail");
+const domicilio = document.getElementById ("domicilio");
+const comentarios = document.getElementById ("comentarios");
+const localidad = document.getElementById ("localidad");
 
+/*procedimiento para gestionar el form */
 
-function agarraform (e){
-  e.preventDefault();  
-  let datosform = new FormData (form);
-  console.log(datosform.get("nombre")); 
-  console.log(datosform.get("apellido")); 
-  console.log(datosform.get("dni")); 
-  console.log(datosform.get("domicilio")); 
-  console.log(datosform.get("telefono")); 
-  console.log(datosform.get("mail")); 
-  console.log(datosform.get("comentarios")); 
-  document.write ("Resumen"+ "<br>");
-  document.write ("Nombre"+ "<br>");
-  document.write(datosform.get("nombre"));
-  document.write ("<br>" + "Apellido"+ "<br>");
-  document.write(datosform.get("apellido"));
-  document.write ("<br>" +"DNI"+ "<br>");
-  document.write(datosform.get("dni")); 
-  document.write ("<br>" +"Domicilioo"+ "<br>");
-  document.write(datosform.get("domicilio")); 
-  document.write ("<br>" +"Teléfono"+ "<br>");
-  document.write(datosform.get("telefono"));
-  document.write ("<br>" +"Correo electrónico"+ "<br>");
-  document.write(datosform.get("mail")); 
-  document.write ("<br>" +"Comentarios"+ "<br>");
-  document.write(datosform.get("comentarios")); 
- 
+const botones = document.getElementsByClassName("btn-form"); //recupera todos los botones de la clase btn-form
+const formulario1 = document.getElementById("paso1");
+const formulario2 = document.getElementById("paso2");
+for(let i = 0; i < botones.length - 1; i++){
+  botones[i].addEventListener("click", handleMultistep);
+
 }
+
+
+function handleMultistep(evento) {
+  evento.preventDefault;
+  // formulario1.classList.toggle("d-none") 
+  // let idboton = evento.target.id;
+   switch (evento.target.id){
+    case 'btn-sig':
+      formulario1.classList.toggle("d-none");
+      formulario2.classList.toggle("d-none");
+      break;
+    case 'btn-ant':
+      formulario1.classList.toggle("d-none");
+      formulario2.classList.toggle("d-none");
+      break;
+    case 'enviar':
+      enviardatos();      
+      break;
+    default:
+      break; 
+  }
+}
+
+
+function enviardatos() {
+   console.log("Hola");
+  // console.log("Apellido y nombre " + nombre.value );
+  // console.log("Telefono" + telefono.value );
+  // console.log("Correo " + mail.value );
+  // console.log("Domicilio " + domicilio.value );
+  // console.log("Localidad " + localidad.value );
+  // console.log("Comentarios " + comentarios.value );
+  // console.log(nombre.value, telefono.value, mail.value, domicilio?.value, localidad.value, comentarios.value);
+  // formulario1.reset();
+  // formulario2.reset();  
+  
+
+}
+
